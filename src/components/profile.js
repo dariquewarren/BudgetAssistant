4ce10f4bf0615693239a91f1d9c5256e4dea1d4a
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-10-30 22:00:57 -0400

    sm chng

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from '../components/logoutButton'
-var axios = require("axios").default;
-
-var options = {
-  method: 'POST',
-  url: 'https://dev-sg8fbv3t.us.auth0.com/oauth/token',
-  mode: 'no-cors',
-  headers: {'content-type': 'application/x-www-form-urlencoded'},
-  data: {
-    grant_type: 'client_credentials',
-    client_id: 'YsUPHTB3zq0gdAuAAd41YYi0ffnwvYEI',
-    client_secret: '3f7EwPwQ0hIgnDAzaatlgzIv6xFmvOg_UkVGYMwoaScVaWp4M17Hh3byYEIp8JbP',
-    audience: 'https://dev-sg8fbv3t.us.auth0.com/api/v2/'
-  }
-};
- if (user){
-    
-  return  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
-  }
 
 const Profile = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [userMetadata, setUserMetadata] = useState(null);
+
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = "dev-sg8fbv3t.us.auth0.com";
+      let myToken
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: "read:current_user",
+        });
+  
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+  
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+  
+        const { user_metadata } = await metadataResponse.json();
+  console.log(accessToken)
+  myToken = accessToken
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    
+       
+    getUserMetadata();
+  }, []);
+  
+  
 
   return (
     isAuthenticated && (
