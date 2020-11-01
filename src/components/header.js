080953cab83ba35ef6db73b8d548efeac3de8837
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-10-31 22:16:01 -0400

    added login and logut buttn to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { NavLink} from 'react-router-dom'
 import Profile from '../components/profile'
+import LoginButton from '../components/loginButton'
+import LogoutButton from '../components/logoutButton'
 
 
 const Header = ()=> (
  
     <header style={{backgroundColor: '#60316e'}}>
-    <Profile/>
-    <h1 style={{color:'#fbe8d3'}} className='text-center'>Budget Buddy</h1>
     
+    <h1 style={{color:'#fbe8d3'}} className='text-center'>Budget Buddy</h1>
+    <LoginButton/>
+    <LogoutButton/>
  <div className='text-center' >
  <NavLink to="/" style={{color: '#29a19c', margin: '1rem'}} exact={true}>Budget Summary</NavLink>
  <NavLink to="/create" style={{color: '#29a19c', margin: '1rem'}} >Create Expense</NavLink>
